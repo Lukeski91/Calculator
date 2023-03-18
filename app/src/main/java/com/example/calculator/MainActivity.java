51a6eb2560b1107bf3f03d8e9d433b6d2d56a969
Calculator <cljuntilla91@gmail.com>
2023-03-18 20:43:17 +0800

    Calculator

---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clearOnClick(View view) {
+    }
+
+    public void bracketsOnClick(View view) {
+    }
+
+    public void powerOfOnClick(View view) {
+    }
+
+    public void divideOnClick(View view) {
+    }
+
+    public void sevenOnClick(View view) {
+    }
+
+    public void eightOnClick(View view) {
+    }
+
+    public void nineOnClick(View view) {
+    }
+
+    public void multiplyOnClick(View view) {
+    }
+
+    public void fourOnClick(View view) {
+    }
+
+    public void fiveOnClick(View view) {
+    }
+
+    public void sixOnClick(View view) {
+    }
+
+    public void minusOnClick(View view) {
+    }
+
+    public void oneOnClick(View view) {
+    }
+
+    public void twoOnClick(View view) {
+    }
+
+    public void threeOnClick(View view) {
+    }
+
+    public void plusOnClick(View view) {
+    }
+}
